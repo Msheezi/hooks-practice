a80e4ef045cb9c5c7894642a3b497c58d6b07fac
msheezi <marshall.randolph@gmail.com>
2019-11-27 15:18:58 -0800

    Counter and custom form hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
+// const expensiveInitialState = () => {
+//   return 10  
+// can use this to set the intial state to the result of this function.  The initial state
+// will only be called once with the usestate instead of on ever render if using a function to define the initial state 
+// }
 const App = () => {
-  return(
+  
+  const [{count, count2}, setCount] = useState({count: 10, count2: 20});
 
-    <div>Hello World</div>
-  )
+return (
+  <>
+  <button onClick={() => setCount(currentState => ({
+    count2: currentState.count2 + 1, 
+    count: currentState.count + 1}))}>+</button>
+    {/* To have multiple updates, spread the object then do the updates to the 
+    portion of the object you want to change.  the updated function doesn't do a 
+    merge.  basically you need to use all the keys in the update to ensure
+    it stays in sync */}
+   
+    <input name="email"></input>
+    <input type="password" name="password"></input>
+
+    <div>Count 1: {count}</div>
+    <div>Count 2: {count2}</div>
+
+
+</>
+) 
+  
 
 }
 
